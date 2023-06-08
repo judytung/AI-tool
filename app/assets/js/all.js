75cc51d590e10eb8da6483900fc0d3a39da9aabd
TUNG HSIAO HAN <sky250331@gmail.com>
2023-06-08 23:03:01 +0800

    add: 1. tailwind.config.js add base screens, fontSize, colors 2. add header html and menu RWD , menu toggle js 3. add base style in all.css 4. add menu.png

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,13 @@
-console.log('Hello!');
+const menu = document.querySelector('#menu');
+const toggleBtn = document.querySelector('#menu-toggle');
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+window.addEventListener('resize', () => {
+  if (document.body.clientWidth > 768) {
+    menu.classList.add('hidden')
+  }
+})
+
+toggleBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  menu.classList.toggle('hidden');
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,44 @@
 module.exports = {
-  content: ["./app/**/*.{html,ejs}"],
+  content: ["./app/**/*.{html,ejs,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true, 
+      padding: "12px"
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1296px',
+    },
+    fontSize: {
+      xs: '12px',
+      sm: '14px',
+      base: '16px',
+      lg: '18px',
+      xl: '20px',
+      '2xl': '24px',
+      '3xl': '28px',
+      '4xl': '32px',
+      '5xl': '40px',
+      '6xl': '48px',
+      '7xl': '80px',
+      '8xl': '120px'
+    },
+    extend: {
+      colors: {
+        'primary': '#F18724',
+        'surface': '#F2EFE7',
+        black : {
+          100: '#020202',
+          80: '#525252',
+          60: '#919191',
+          40: '#D1D1D1',
+          20: '#F2F2F2',
+          0: '#FFFFFF'
+        }
+      }
+    },
   },
   plugins: [
   ],
