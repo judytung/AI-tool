cdfa62912d8ae68b56d6442306e21ddde95520ff
TUNG HSIAO HAN <sky250331@gmail.com>
2023-06-08 23:20:52 +0800

    Update 2023-06-08T15:20:50.310Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,14 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+var menu = document.querySelector('#menu');
+var toggleBtn = document.querySelector('#menu-toggle');
+window.addEventListener('resize', function () {
+  if (document.body.clientWidth > 768) {
+    menu.classList.add('hidden');
+  }
+});
+toggleBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  menu.classList.toggle('hidden');
 });
 //# sourceMappingURL=all.js.map
