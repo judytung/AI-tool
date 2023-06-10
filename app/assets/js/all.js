66d9b2ede5b7f4989752eedc5bb4f9b665d48ca0
TUNG HSIAO HAN <sky250331@gmail.com>
2023-06-10 17:36:30 +0800

    1. add: html / footer 2. add: images / close.png and arrow upward.png 3. add: js /  menu 切換、go top

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,13 +1,39 @@
 const menu = document.querySelector('#menu');
 const toggleBtn = document.querySelector('#menu-toggle');
+const goTop = document.querySelector('#goTop');
+const main = document.querySelector('.main');
 
 window.addEventListener('resize', () => {
-  if (document.body.clientWidth > 768) {
-    menu.classList.add('hidden')
+  if (document.body.clientWidth > 1024) {
+    menu.classList.add('hidden');
+    toggleBtn.innerHTML = `<img src="./assets/images/menu.png" alt="三條白線，水平並排">`;
+    goTop.classList.remove('hidden');
   }
-})
+});
+
+goTop.addEventListener('click', goToTop);
 
 toggleBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  menu.classList.toggle('hidden');
+  const icon = e.target.closest('a').dataset.icon;
+  if (icon === 'open') {
+    menu.classList.remove('hidden');
+    toggleBtn.setAttribute("data-icon", "close");
+    toggleBtn.innerHTML = `<img src="./assets/images/close.png" alt="兩題白線交叉，形成Ｘ狀">`;
+    main.classList.add('hidden');
+    goTop.classList.add('hidden');
+  } else {
+    menu.classList.add('hidden');
+    toggleBtn.setAttribute("data-icon", "open");
+    toggleBtn.innerHTML = `<img src="./assets/images/menu.png" alt="三條白線，水平並排">`;
+    main.classList.remove('hidden');
+    goTop.classList.remove('hidden');
+  }
 });
+
+
+let goToTop = (e) => {
+  e.preventDefault();
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
