42db22489e78e697d76e0468101631d46a496db2
TUNG HSIAO HAN <sky250331@gmail.com>
2023-06-11 12:35:43 +0800

    Update 2023-06-11T04:35:41.485Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -59,4 +59,39 @@ var swiper = new Swiper(".cardSwiper", {
   }
 });
 /* get ai tool data */
+
+var path = 'https://2023-engineer-camp.zeabur.app';
+var list = document.querySelector('#ai-tool-list');
+var worksData = [];
+var pagesData = {};
+var data = {
+  type: '',
+  sort: 0,
+  page: 1,
+  search: ''
+};
+
+function getData(_ref) {
+  var type = _ref.type,
+      sort = _ref.sort,
+      page = _ref.page,
+      search = _ref.search;
+  var url = "".concat(path, "/api/v1/works?sort=").concat(sort, "&page=").concat(page, "&").concat(type ? "type=".concat(type, "&") : '').concat(search ? "search=".concat(search) : '');
+  axios.get(url).then(function (res) {
+    worksData = res.data.ai_works.data;
+    pagesData = res.data.ai_works.page;
+    renderWorks();
+    console.log(pagesData);
+  });
+}
+
+getData(data);
+
+function renderWorks() {
+  var result = '';
+  worksData.forEach(function (item) {
+    result += "<li class=\"card flex flex-col border border-black-20 rounded-t-2xl w-full md:w-[48%] lg:w-[32%] overflow-hidden cursor-pointer\">\n    <div class=\"overflow-hidden\">\n      <img src=\"".concat(item.imageUrl, "\" alt=\"").concat(item.title, "\" class=\"w-full\">\n    </div>\n    <div class=\"py-5 px-8 flex-grow\">\n      <h3 class=\"text-black-100 text-xl font-black\">").concat(item.title, "</h3>\n      <p class=\"text-black-80 text-sm leading-normal mt-3\">").concat(item.description, "</p>\n    </div>\n    <div class=\"flex justify-between border-t border-black-20 py-5 px-8\">\n      <span class=\"text-black-100 leading-6 font-bold\">AI \u6A21\u578B</span>\n      <span class=\"text-black-100 leading-6\">").concat(item.model, "</span>\n    </div>\n    <div class=\"flex justify-between border-t border-black-20 py-5 px-8\">\n      <span class=\"text-black-100 leading-6\">#").concat(item.type, "</span>\n      <a href=\"#\">\n        <span class=\"material-icons text-black-100\">share</span>\n      </a>\n    </div>\n  </li>");
+  });
+  list.innerHTML = result;
+}
 //# sourceMappingURL=all.js.map
